ab5ccee2bf0e82996e8c47babf9234b088f98a76
Gaurav jain <gjain7764@gmail.com>
2026-08-21 17:04:56 +0530

    still learning

---
+++ Learning/variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+    // var name string = "Gaurav"
+    var name= "Gaurav"
+    var isAdult = true
+    fmt.Println(isAdult)
+    fmt.Println(name)
+
+    var age int = 23
+    fmt.Println(age)
+}
