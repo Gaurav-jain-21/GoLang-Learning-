2ae87f0f3025a8aaaa536774d6c83e53312e7cdd
Gaurav jain <gjain7764@gmail.com>
2026-08-24 22:36:04 +0530

    this is my commit

---
+++ DS/myMaps.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps in golang")
+
+	language:= make(map[string]string)
+	language["js"]="JavaScript"
+	language["py"]="Python"
+	language["RB"]="Ruby"
+	language["Java"]="java"
+
+	fmt.Println(language)
+	fmt.Println("js short for: ",language["js"])
+}
